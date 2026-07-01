b82549aa11923f50c2606e851a2b3d2fc9469e35
Shardul Barve <shardulb16@gmail.com>
2026-07-01 18:16:47 +0530

    C++ program that demonstrates how to obtain and print the memory addresses of variables using the address-of operator.

---
+++ Pointers/address_of_variables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+
+int main(){
+    int a = 10;
+    float b = 672.722;
+    double d = 4564.334293;
+    std::cout<<"The address of a is : "<<&a<<"\n";
+    std::cout<<"The address of b is : "<<&b<<"\n";
+    std::cout<<"The address of d is : "<<&d<<"\n";
+
+
+
+
+
+    return 0;
+}
