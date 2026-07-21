2b7fec245b57aee991b21cd8563c4820e2153756
Shardul Barve <shardulb16@gmail.com>
2026-07-22 01:22:14 +0530

    Demonstrated the override specifier in C++ by correctly overriding a virtual function in a derived class. Explored how override enables compile-time verification of virtual function overrides, preventing accidental signature mismatches and improving code safety.

---
+++ Ovveride and final /Override Function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Animal {
+public:
+    virtual void speak() {
+        std::cout << "Animal speaks" << std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void speak() override {
+        std::cout << "Dog barks" << std::endl;
+    }
+};
+
+int main() {
+    Animal* animal = new Dog();
+    animal->speak();
+
+    delete animal;
+
+    return 0;
+}
