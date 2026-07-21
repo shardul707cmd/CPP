6d9c32406f33b0289b8efc3e899bb3cd094ebfa5
Shardul Barve <shardulb16@gmail.com>
2026-07-22 02:43:51 +0530

    Demonstrated the use of a virtual destructor in a polymorphic base class. Verified that deleting a derived object through a base-class pointer correctly invokes both the derived and base destructors, ensuring complete object destruction and proper resource cleanup.

---
+++ Virtual Destructors /Virtual Destructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Animal {
+public:
+    virtual void speak() {
+        std::cout << "Animal speaks" << std::endl;
+    }
+    virtual ~Animal() {
+        std::cout << "Animal destroyed" << std::endl;
+    }
+
+};
+
+class Dog : public Animal {
+public:
+    void speak() override final {
+        std::cout << "Dog barks" << std::endl;
+    }
+    ~Dog() {
+        std::cout << "Dog destroyed" << std::endl;
+    }
+};
+
+
+
+int main() {
+    Animal* animal = new Dog();
+    animal->speak();
+
+    delete animal;
+
+    return 0;
+}
