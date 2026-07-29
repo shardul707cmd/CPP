d2c70afd84c360f7bd0bd651ca8356029a7944ed
Shardul Barve <shardulb16@gmail.com>
2026-07-29 15:26:33 +0530

    Demonstrated shared ownership semantics of std::shared_ptr by creating multiple owners within different scopes. Observed how the reference count increases as new owners are created and automatically decreases when owners leave scope, illustrating RAII-based lifetime management and automatic resource cleanup.

---
+++ Smart Pointers/Shared Ownership.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<memory>
+
+
+int main (){
+    auto ptr = std::make_shared<int>(14);
+    auto ptr2 = ptr;
+    std::cout<<ptr.use_count()<<"\n";
+    {
+    auto ptr3 = ptr;
+    auto ptr4 = ptr;
+    std::cout<<ptr.use_count()<<"\n";
+    }
+    std::cout<<ptr.use_count()<<"\n";
+    return 0;
+}
